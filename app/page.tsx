c3097315229ed756d203953268010b747411b3bc
fahad <fahad@bluecast.tech>
2025-11-23 23:29:36 +0530

    route

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { LocalStorage } from "@/utility/localstorage";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
+  const token = LocalStorage.getItem("accessToken");
   const router = useRouter();
   useEffect(() => {
-    router.push("/auth/login");
-  }, []);
+    if (token) {
+      router.push("/questionnaire/start");
+    } else {
+      router.push("/auth/login");
+    }
+  }, [token]);
   return <></>;
 }
